0937541f7e38c8adaeeea8d83784704e62b4de41
System Administrator <root@iMac-Abraham.local>
2019-05-26 01:25:04 -0500

    integracion de react

--- app/qoutes/App.js
+++ app/qoutes/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+           <View style={{height: 25, backgroundColor: 'powderblue'}} />
+           <View style={{height: 100, backgroundColor: 'powderblue'}} >
+            <Text style={styles.styleTitle}>Quotes {"\n"}By React Natine</Text>
+           </View>
+           <View style={styles.styleTarget} >
+             <Text style={styles.styleMsj}>Hola</Text>
+           </View>
+         <Button
+            title="Learn More"
+           color="#841584"
+           accessibilityLabel="Learn more about this purple button"
+         />
       </View>
     );
   }
@@ -13,9 +24,32 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     flex: 1,
+     flexDirection: 'column',
+     justifyContent: 'center',
+     alignItems: 'stretch',
   },
+  styleText:{
+    color:'white',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    fontSize: 30,
+     marginTop: 10
+  },
+  styleMsj:{
+      color:'white',
+      fontSize: 20,
+      padding: 10
+  },
+   styleTitle:{
+          fontSize: 30,
+          paddingLeft: 30,
+      },
+  styleTarget: {
+       flex: 1,
+       backgroundColor: '#FC8970',
+       borderRadius: 12,
+       margin:10,
+       padding:20
+    }
 });
---
+++ app/src/main/java/com/abrsoftware/speeddytest/view/detailqoute/DetailQouteReact.java
@@ -0,0 +1,4 @@
+package com.abrsoftware.speeddytest.view.detailqoute;
+
+public class DetailQouteReact {
+}
